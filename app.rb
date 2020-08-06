c4baa0de586994d0d345a0317790ef1c2ebaaaa0
Slaries <slaries@rambler.ru>
2020-08-06 18:47:03 +0300

    add sqllite3 db

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do
+	@db = SQLite3::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS 
+	"Users" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"username"	TEXT,
+	"phone"	TEXT,
+	"datestamp"	TEXT,
+	"barber"	TEXT,
+	"color"	TEXT)'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -20,7 +33,7 @@ post '/visit' do
 	@username = params[:username]
 	@phone = params[:phone]
 	@datetime = params[:datetime]
-	@baber = params[:baber]
+	@barber = params[:barber]
 	@color = params[:color]
 
 	hh = {  :username => 'Введите имя', 
