0ce8ada4bdd7f56495c9e7d1b884937878b3785c
Slaries <slaries@rambler.ru>
2020-08-07 14:06:35 +0300

    init barbers table config

--- app.rb
+++ app.rb
@@ -7,20 +7,48 @@ def get_db
 		@db = SQLite3::Database.new 'barbershop.db'
 		@db.results_as_hash = true
 		return @db
-		
+
+end
+def is_barber_exists? db, name
+	db.execute('SELECT * FROM Barbers WHERE name=?', [name]).length > 0
+
+end
+
+def seed_db db, barbers
+
+	barbers.each do |x|
+		if !is_barber_exists? db, x
+			db.execute 'INSERT INTO Barbers (name) values (?)', [x]
+		end
 	end
+
+end
 
 configure do
 
 	db = get_db
 	db.execute 'CREATE TABLE IF NOT EXISTS 
-	"Users" (
+	"Users" 
+	(
 	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	"username"	TEXT,
 	"phone"	TEXT,
 	"datestamp"	TEXT,
 	"barber"	TEXT,
-	"color"	TEXT)'
+	"color"	TEXT
+	)'
+
+	db.execute 'CREATE TABLE IF NOT EXISTS 
+	"Barbers" 
+	(
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"name"	TEXT
+	)'
+
+
+	seed_db db, ['Foo', 'Faa', 'Moo', 'Zoo', 'Faz', 'Maz', 'Kraz']
+
+  
 end
 
 get '/' do
